1f349acf04134533fa742cbf27f886b745627207
PoojithaYarava <2303A51499@sru.edu.in>
2026-03-26 00:32:30 +0530

    Added register API with password hashing

---
+++ server/controllers/authController.js
@@ -0,0 +1,25 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+
+exports.register = async (req, res) => {
+  try {
+    const { name, email, password, role } = req.body;
+
+    // hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = new User({
+      name,
+      email,
+      password: hashedPassword,
+      role
+    });
+
+    await user.save();
+
+    res.json({ message: "User registered successfully" });
+
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
--- server/server.js
+++ server/server.js
@@ -1,14 +1,28 @@
 const express = require("express");
-const app = express();
-const cors = require("cors");
+const mongoose=require("mongoose");
+const cors=require("cors");
 
+const app = express();
+
+//middleware
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
+//MongoDB Connection
+mongoose.connect("mongodb+srv://yaravapoojitha_db_user:Poojitha_003@healthcare-cluster.ng6kkur.mongodb.net/healthcare?retryWrites=true&w=majority")
+.then(()=> console.log("MongoDB connected"))
+.catch(err => console.log(err));
+
+//routes
+const authRoutes = require("./routes/authRoutes");
+app.use("/api/auth",authRoutes);
+
+//test route
+app.get("/",(req,res)=>{
   res.send("API running");
 });
 
+//server
 app.listen(5000, () => {
   console.log("Server running on port 5000");
 });
