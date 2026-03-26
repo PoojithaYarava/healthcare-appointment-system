205b9d65b7d64e2cf61bbcadcf1a73e85288b59a
PoojithaYarava <2303A51499@sru.edu.in>
2026-03-26 12:03:27 +0530

    Added login API with JWT authentication

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
-
+const jwt = require("jsonwebtoken");
+//REGISTER
 exports.register = async (req, res) => {
   try {
     const { name, email, password, role } = req.body;
@@ -22,4 +23,34 @@ exports.register = async (req, res) => {
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
+};
+
+//LOGIN
+exports.login = async(req, res)=>{
+  try{
+    const {email,password}=req.body;
+    const user = await User.findOne({email});
+
+    if (!user){
+      return res.status(400).json({message: "User not found"});
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch){
+      return res.status(400).json({message: "Invalid password"});
+  }
+  const token = jwt.sign(
+    {id: user._id},
+    "secretKey",
+    {expiresIn:"1d"}
+  );
+  res.json({
+    message: "Login successful",
+    token
+  });
+
+} catch (error){
+  res.status(500).json({ error: error.message });
+}
 };
